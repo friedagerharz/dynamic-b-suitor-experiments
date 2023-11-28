a26aefd621667ab59fe2e86857ed100beb8ffa19
Frieda Gerharz <frieda.gerharz@informatik.hu-berlin.de>
2023-11-28 16:11:50 +0100

    use r-mat generator for graph generation

--- generate_random_graph.py
+++ generate_random_graph.py
@@ -4,39 +4,34 @@ import networkit as nk
 import os
 import random
 
-def generate(n, prob):
+def generate(sc, fc, a, b, c, d, suf):
     input_dir = 'instances'
     os.makedirs(input_dir, exist_ok=True)
-    file_name = f"er-{len(str(n)) - 1}.nkb"
+    file_name = f"rmat-{suf}.nkb"
     dest_path = os.path.join(input_dir, file_name)
 
     if os.path.isfile(dest_path):
         print(f"{file_name} already exists.")
         return
     
-    G = nk.generators.ErdosRenyiGenerator(n, prob).generate()
+    G = nk.generators.RmatGenerator(sc, fc, a, b, c, d, True).generate()
     G.removeMultiEdges()
     G.removeSelfLoops()
-    if G.isDirected():
-        G = nk.graphtools.toUndirected(G)
     nk.graphtools.randomizeWeights(G)
     nk.graphio.NetworkitBinaryWriter().write(G, dest_path)
 
-    # # remove:
-    # GR = nk.graphio.NetworkitBinaryReader().read(dest_path)
-    # print(f"nodes: {G.numberOfNodes()}")
-    # print(f"edges: {G.numberOfEdges()}")
-
-    # def printEdge(v, u, w, eid):
-    #     print(f"({v},{u}) - {w}")
-    # GR.forEdges(printEdge)
-
 if __name__ == "__main__":
-    prob = 0.2
-    num_nodes = [10**3, 10**4, 10**5, 10**6, 10**7]
+    sc = 20
+    fc = 8
+    a_probs = [0.55, 0.45, 0.25]
+    b_probs = [0.15, 0.15, 0.25]
+    c_probs = [0.15, 0.15, 0.25]
+    d_probs = [0.15, 0.25, 0.25]
+    suffix = ["b", "g", "er"]
+
     seed = 1
     nk.setSeed(seed, False)
     random.seed(seed)
 
-    for n in num_nodes:
-        generate(n,prob)
+    for a, b, c, d, suf in zip(a_probs, b_probs, c_probs, d_probs, suffix):
+        generate(sc, fc, a, b, c, d, suf)
